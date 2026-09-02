1a763481c91a3a3a1508d949fccc402963aaf1ec
Anurag Solanki <anuragsolanki0223@gmail.com>
2026-09-02 22:02:27 +0530

    Time: 2 ms (10.62%) | Memory: 8.5 MB (47.80%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string a, b;
+
+        for (char c : s) {
+            if (c == '#') {
+                if (!a.empty())
+                    a.pop_back();
+            } else {
+                a.push_back(c);
+            }
+        }
+
+        for (char c : t) {
+            if (c == '#') {
+                if (!b.empty())
+                    b.pop_back();
+            } else {
+                b.push_back(c);
+            }
+        }
+
+        return a == b;
+    }
+};
